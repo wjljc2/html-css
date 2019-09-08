3813a4995bbc3792b8265330a929114e9771eb39
lijiacong-coder <1143596375@qq.com>
2019-09-08 14:40:09 +0800

    vue/cli

---
+++ 董老师课上练习1/test2/src/components/CommonHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <img src="./../assets/img/树叶4.png" alt="">
+        hhhhh
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
