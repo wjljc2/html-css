edc986aa19927a7407dc631646e8cdf1d6d4e855
lijiacong-coder <1143596375@qq.com>
2019-09-28 14:43:43 +0800

    vueapp

--- 董老师课上练习1/vue-app/src/views/Movie.vue
+++ 董老师课上练习1/vue-app/src/views/Movie.vue
@@ -7,10 +7,11 @@
                     <h2>{{obj.original_title}}</h2>
                     <p>{{obj.collect_count}}已收藏</p>
                     <p>演员:<span v-for="(actor,index) in obj.casts" :key="index">{{actor.name}} </span></p>
-                    <p>上映时间:{{obj.mainland_pubdate}}</p>
+                    <p>上映时间:{{obj.year}}</p>
                 </div>
             </li>
         </ul>
+        <img class = "loading" v-show="isShow" src="@/assets/img/loading.gif" alt="">
     </div>
 </template>
 
@@ -19,20 +20,46 @@
     export default {
         data(){
             return{
-                movieList:[]
+                movieList:[],
+                isShow:false,
+                flag:true
+            }
+        },
+        methods:{
+            handScroll(){
+                var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+                var windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
+                var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+                if(scrollTop + windowHeight - scrollHeight > -1 && this.flag == true){
+                    this.isShow=true;
+                    this.flag = false;
+                    axios.get('./data/movie10.json')
+                    .then((res)=>{
+                        console.log(res.data);
+                        this.movieList = [...this.movieList,...res.data.subjects];
+                        this.isShow=false;
+                        this.flag = true;
+                    }).catch((res)=>{
+                        console.log('aaa')
+                    })
+                }
             }
         },
         created(){
             // 跨域 域名(baidu.com) 端口(8080 8081) 协议(http https) 有一个不同即为跨域 ，都相同才是同源
             // 解决跨域 第三方服务器代理 jsonbird
-            axios.get('https://bird.ioliu.cn/v1?url=https://douban.uieee.com/v2/movie/in_theaters?start=0&count=10')
+            // axios.get('https://bird.ioliu.cn/v1?url=https://douban.uieee.com/v2/movie/in_theaters?start=0&count=10')
+            axios.get('./data/movie0.json')
             .then((res)=>{
                 console.log(res.data);
                 this.movieList = res.data.subjects;
             }).catch((res)=>{
                 console.log('aaa');
             })
-        }
+        },
+        mounted(){
+            window.addEventListener('scroll',this.handScroll,true);
+        },
     }
 </script>
 
@@ -48,5 +75,12 @@
         margin-bottom: 0.2rem;
         border-bottom: 1px solid #000;
     }
+}
+.loading{
+    width: 3rem;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
 }
 </style>
