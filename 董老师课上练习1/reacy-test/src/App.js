a8040dcfcd97e24a0b26523156b08b1e7284b9d6
lijiacong-coder <1143596375@qq.com>
2019-10-26 16:28:21 +0800

    test react

--- 董老师课上练习1/reacy-test/src/App.js
+++ 董老师课上练习1/reacy-test/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +20,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ 董老师课上练习1/reacy-test/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                xia
+            </div>
+        );
+    }
+}
+
+export default Footer;
